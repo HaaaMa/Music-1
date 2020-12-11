edf6b9423da55e2508e543df3fcc3fa37b0957f7
level09 <72306257+level976@users.noreply.github.com>
2020-12-12 01:20:38 +0300

    Update invite.js

--- others/invite.js
+++ others/invite.js
@@ -7,16 +7,16 @@ const {
 
 module.exports = {
   name: `invite`,
-  description: `Gives you an invite`,
-  aliases: ["add"],
+  description: "__**```(inv)Gives you an invite```**__",
+  aliases: ["inv"],
   cooldown: 3,
   edesc: "Type this command to get an invite link for the Bot, thanks for every Invite",
   execute(message, args, client) {
     //react with approve emoji
     message.react("769665713124016128");
     //send the invite embed
-    message.reply(new MessageEmbed().setColor("#c219d8").setTitle(":heart: Please Invite me: ").setDescription("https://discord.com/oauth2/authorize?client_id=769642999227351070&permissions=0&scope=bot")
-    .setFooter("And enjoy listening to music!", "https://cdn.discordapp.com/avatars/769642999227351070/f1b78891507308fb76c0a66b56f4bcd6.webp?size=512"));
+    message.reply(new MessageEmbed().setColor("#c219d8").setTitle("__**🖤Please Invite me**__ ").setDescription("https://discord.com/oauth2/authorize?client_id=769642999227351070&permissions=0&scope=bot")
+    .setFooter("And enjoy listening to music!", "https://discord.com/api/oauth2/authorize?client_id=783307683238510643&permissions=8&scope=bot"));
 
   }
 }
