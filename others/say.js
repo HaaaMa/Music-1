73262d35b22f0ebb8ff082c49e44700fe955dc0a
level09 <72306257+level976@users.noreply.github.com>
2020-12-13 00:16:36 +0300

    Update say.js

--- others/say.js
+++ others/say.js
@@ -1,1 +1,11 @@
+const { Client, Collection, MessageEmbed } = require(`discord.js`);
+const {
+  PREFIX,
+  approveemoji,
+  denyemoji
+} = require(`../config.json`);
 
+module.exports = {
+  name: `say`,
+  description: "__**```(say)say bot```**__",
+  aliases: [""], }
