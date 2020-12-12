65464639c16702a8977df69034267165e8c587ff
level09 <72306257+level976@users.noreply.github.com>
2020-12-13 00:19:12 +0300

    Update say.js

--- others/say.js
+++ others/say.js
@@ -1,9 +1,3 @@
-const { Client, Collection, MessageEmbed } = require(`discord.js`);
-const {
-  PREFIX,
-  approveemoji,
-  denyemoji
-} = require(`../config.json`);
 
 module.exports = {
   name: `say`,
