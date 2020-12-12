41a763ac89c689b0833176182907e78d2e29b114
level09 <72306257+level976@users.noreply.github.com>
2020-12-12 17:51:24 +0300

    Update play.js

--- include/play.js
+++ include/play.js
@@ -161,7 +161,7 @@ module.exports = {
 
     try {
       const newsong = new MessageEmbed()
-        .setTitle("<:Playing:769665713124016128><​a:emoji_1:782443655336034355>")
+        .setTitle("<:Playing:769665713124016128>")
         .setURL(song.url)
         .setColor("#c219d8")
         .setThumbnail(thumb)
