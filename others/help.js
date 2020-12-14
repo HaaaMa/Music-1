40119a7c0cb973dc6c3c0fa01e0031d453d80beb
level09 <72306257+level976@users.noreply.github.com>
2020-12-14 16:53:48 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -24,9 +24,9 @@ module.exports = {
     let helpEmbed = new MessageEmbed()
       .setTitle(`prefix bot =`)
       .setDescription("__**```List of all commands```**__")
-       .setImage('https://cdn.discordapp.com/attachments/735323677129965588/787445135118041119/tenor-5.gif')
+       .setImage('https://cdn.discordapp.com/attachments/782168543076155403/788038115666559006/image0.jpg')
        .setFooter(message.author.username, message.author.displayAvatarURL)
-       .setDescription(`**[  SUPPORT  ](https://discord.gg/AQgmEJ2)**   -  [   INVITE   ](https://discord.com/api/oauth2/authorize?client_id=681874861441810453&permissions=8&scope=bot)`)
+       .setDescription(`**[  SUPPORT  ](https://discord.gg/AQgmEJ2)**   -  [   INVITE   ](https://discord.com/api/oauth2/authorize?client_id=783307683238510643&permissions=8&scope=bot)`)
       .setThumbnail(message.author.avatarURL)
       .setColor('RANDOM');
     //define argstrue to negative
