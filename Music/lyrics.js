e591c741e4935c438fa804aed6fd23e7ce3be6c9
level09 <72306257+level976@users.noreply.github.com>
2020-12-15 01:28:47 +0300

    Update lyrics.js

--- Music/lyrics.js
+++ Music/lyrics.js
@@ -12,7 +12,7 @@ const { PREFIX } = require(`../config.json`);
 module.exports = {
   name: "lyrics",
   aliases: ["ly", "text"],
-  description: "__**```(ly)Get lyrics for the currently playing song```**__",
+  description: "(ly)Get lyrics for the currently playing song",
   cooldown: 7.5,
   edesc: `Type the Command while listening to a song, to get the lyrics from!\nUsage: ${PREFIX}lyrics`,
 
