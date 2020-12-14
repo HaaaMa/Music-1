85661b85e4e0ca1e014ba1647f9fd0db2c20e640
level09 <72306257+level976@users.noreply.github.com>
2020-12-15 01:34:29 +0300

    Update stop.js

--- Music/stop.js
+++ Music/stop.js
@@ -10,7 +10,7 @@ const { PREFIX } = require(`../config.json`);
 ////////////////////////////
 module.exports = {
   name: "stop",
-  description: "__**```(leave)Stops the music```**__",
+  description: "(leave)Stops the music",
   aliases: ["leave", "end"],
   cooldown: 5,
   edesc: `Type the Command, to stop playing and leave the channel.\nUsage: ${PREFIX}stop`,
