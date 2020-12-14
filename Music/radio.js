b7dfc40fcf7f74a093eec1d4ca25d3e429adb6a5
level09 <72306257+level976@users.noreply.github.com>
2020-12-15 01:31:09 +0300

    Update radio.js

--- Music/radio.js
+++ Music/radio.js
@@ -64,7 +64,7 @@
   module.exports = {
     name: "radio",
     aliases: ['ro'],
-    description: "__**```(ro)Play a Radiostation```**__",
+    description: "(ro)Play a Radiostation",
     cooldown: 3,
     edesc: `Type this command to play a radio live stream!\nUsage: ${PREFIX}radio <1-34>`,
   async execute(message, args, client) {
