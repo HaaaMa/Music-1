506ea944679f7bd6a92a9ad68fe3642af2706da5
wolfwolf12 <79043462+wolfwolf12@users.noreply.github.com>
2021-02-14 14:05:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('');
+client.login('ODA2ODQwMjEyNjA4OTA5MzQ0.YBvSgQ.KaNLfsOGd2pGd7P2Yk2eFIWK0Ic');
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
