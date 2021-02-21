a6f6e3d2edae41fcde38b47392fddb22ea34cba6
wolfwolf12 <79043462+wolfwolf12@users.noreply.github.com>
2021-02-21 12:45:40 +0300

    Update volume.js

--- Music/volume.js
+++ Music/volume.js
@@ -11,9 +11,9 @@ const { PREFIX } = require(`../config.json`);
 module.exports = {
   name: "volume",
   aliases: ["v"],
-  description: "(v)Change volume0/200",
+  description: "(v)Change volume0/1000",
   cooldown: 5,
-  edesc: `Type the Command, to change the volume of the current song.\nUsage: ${PREFIX}volume <0-200>`,
+  edesc: `Type the Command, to change the volume of the current song.\nUsage: ${PREFIX}volume <0-1000>`,
 
 execute(message, args) {
     //if not a guild return
@@ -33,10 +33,10 @@ execute(message, args) {
     //if no args return info embed
     if (!args[0]) return message.channel.send(volinfoembed).catch(console.error);
     //if args is not a number return error
-    if (isNaN(args[0])) return attentionembed(message,"That's not a Number between **0 & 200**");
+    if (isNaN(args[0])) return attentionembed(message,"That's not a Number between **0 & 1000**");
     //if args is not a Number between 150 and 0 return error
-    if (parseInt(args[0]) < 0 || parseInt(args[0]) > 200)
-      return attentionembed(message,"That's not a Number between **0 & 200**");
+    if (parseInt(args[0]) < 0 || parseInt(args[0]) > 1000)
+      return attentionembed(message,"That's not a Number between **0 & 1000**");
     //set queue volume to args
     queue.volume = args[0];
     //set current volume to the wanted volume
