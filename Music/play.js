015ab724e6945c46d85fcb977bd4370334c861ae
wolfwolf12 <79043462+wolfwolf12@users.noreply.github.com>
2021-02-22 19:06:18 +0300

    Update play.js

--- Music/play.js
+++ Music/play.js
@@ -159,7 +159,7 @@ async execute(message, args, client) {
       serverQueue.songs.push(song);
       //the new song embed
       const newsong = new MessageEmbed()
-        .setTitle("<:Playing:769665713124016128> " + song.title)
+        .setTitle("<:emoji_5:813090579810484284>" + song.title)
         .setColor("#c219d8")
         .setThumbnail(thumb)
         .setURL(song.url)
