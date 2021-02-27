5fdb8e010d9e1daa19be70d3a4fd630781ecbfce
wolfwolf12 <79043462+wolfwolf12@users.noreply.github.com>
2021-02-27 14:41:56 +0300

    Update volume.js

--- Music/volume.js
+++ Music/volume.js
@@ -28,7 +28,7 @@ execute(message, args) {
     if (!canModifyQueue(message.member)) return;
     //define Info Embed
     const volinfoembed = new MessageEmbed()
-    .setColor("#c219d8")
+    .setColor("RED")
     .setTitle(`🔊 Volume is: \`${queue.volume}%\``)
     //if no args return info embed
     if (!args[0]) return message.channel.send(volinfoembed).catch(console.error);
@@ -43,7 +43,7 @@ execute(message, args) {
     queue.connection.dispatcher.setVolumeLogarithmic(args[0] / 100);
     //define approve embed
     const volinfosetembed = new MessageEmbed()
-    .setColor("#c219d8")
+    .setColor("RED")
     .setTitle(`🔊 Volume changed to: \`${args[0]}%\`!`)
     //Send approve message
     return queue.textChannel.send(volinfosetembed).catch(console.error);
