0e5fc0bb26dce3464dea6bd574a23e252c812d48
wolfwolf12 <79043462+wolfwolf12@users.noreply.github.com>
2021-02-27 14:07:09 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -20,44 +20,24 @@ module.exports = {
     .setImage('')
     .setDescription(`
 
- ≪ Prefix Bot . ≫
+ User Commands
+**.invite - .support - .about
+.ping - .prefix - .uptime**
 
-      <a:emoji_11:814035817253175316> command music <a:emoji_11:814035817253175316>
+Music Commands
+**.play - .skip - .skipto
+.stop - .volume - .nowplaying
+.suffle - .search - .resume
+.remove - .queue - .filter
+.loop - .lyric - .radio**
 
- <a:emoji_7:814035064635129907> .loop 
- <a:emoji_7:814035064635129907> .lyrics              
- <a:emoji_7:814035064635129907> .pause
- <a:emoji_7:814035064635129907> .play                   
- <a:emoji_7:814035064635129907> .queue
- <a:emoji_7:814035064635129907> .radio                 
- <a:emoji_7:814035064635129907> .remove
- <a:emoji_7:814035064635129907> .resume             
- <a:emoji_7:814035064635129907> .search
- <a:emoji_7:814035064635129907> .shuffle              
- <a:emoji_7:814035064635129907> .skip
- <a:emoji_7:814035064635129907> .skipto               
- <a:emoji_7:814035064635129907> .stop
- <a:emoji_7:814035064635129907> .volume
-
-      <a:emoji_7:814035032704155728> Filter Commands <a:emoji_7:814035032704155728>
- <a:emoji_10:814035106293088267> .fi 8D
- <a:emoji_10:814035106293088267> .fi tremolo
- <a:emoji_10:814035106293088267> .fi vibrato
- <a:emoji_10:814035106293088267> .fi pulsator
- <a:emoji_10:814035106293088267> .fi clear
- <a:emoji_10:814035106293088267> .fi bassboost
- <a:emoji_10:814035106293088267> .fi vaporwave
-
-      <a:emoji_13:814037864479981578> Others <a:emoji_13:814037864479981578>
- <a:emoji_8:814035085400735755> .help              <a:emoji_8:814035085400735755> .ping
- <a:emoji_8:814035085400735755> .prefix             <a:emoji_8:814035085400735755> .uptime
-
+Links
 [support](https://discord.gg/jcs4XwcExv)    -    [invite](https://discord.com/oauth2/authorize?client_id=806840212608909344&permissions=70346817&scope=bot)
 
 `)
 
   
-   .setColor("BLUE");
+   .setColor("RED");
    helpEmbed.setTimestamp();
    message.react("<a:emoji_11:814035839956418570>")
     return message.channel.send(helpEmbed).catch(console.error);
