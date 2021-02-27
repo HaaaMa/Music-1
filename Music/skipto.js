7f40306b0a3d9be3eb9fc5ac756f8093ef12a208
wolfwolf12 <79043462+wolfwolf12@users.noreply.github.com>
2021-02-27 14:40:40 +0300

    Update skipto.js

--- Music/skipto.js
+++ Music/skipto.js
@@ -52,7 +52,7 @@ execute(message, args) {
     //Send approve
     queue.textChannel.send(
       new MessageEmbed()
-        .setColor("#c219d8")
+        .setColor("RED")
         .setAuthor(`${message.author.username}#${message.author.discriminator} skipped ${args[0]} songs`, "https://cdn.discordapp.com/emojis/769915194444480542.png")
     ).catch(console.error);
   }
