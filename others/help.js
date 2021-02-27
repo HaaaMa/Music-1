4b09b397daf021cf73c2ea99714823b3ff2f42e0
wolfwolf12 <79043462+wolfwolf12@users.noreply.github.com>
2021-02-27 21:50:43 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -6,38 +6,39 @@ const {
 } = require(`../config.json`);
 const db = require('quick.db');
 
+
 module.exports = {
   name: "help",
   aliases: ["h"],
   cooldown: 8,
-  description: "all commands",
+  description: "**all commands**",
   execute(message) {
     let commands = message.client.commands.array();
 
     let helpEmbed = new MessageEmbed()
     .setThumbnail(message.author.avatarURL())
-    .setImage('')
-    .setDescription(``)
+    .setImage('https://cdn.discordapp.com/attachments/748984375772840046/800487276173328445/f7179eb460f2925c2edab10872a3c6f6.gif')
+    .setDescription(`
 
-User Commands
+ User Commands
 `.invite` - `.support` - `.about`
 `.ping` - `.prefix` - `.uptime`
-
 Music Commands
 `.play` - `.skip` - `.skipto`
-`.stop` - `.volume` - `.nowplaying`
+`.stop` - `.volume` - `nowplaying`
 `.suffle` - `.search` - `.resume`
 `.remove` - `.queue` - `.filter`
 `.loop` - `.lyric` - `.radio`
 
 Links
-[support](https://discord.gg/jcs4XwcExv)    -    [invite](https://discord.com/oauth2/authorize?client_id=806840212608909344&permissions=70346817&scope=bot)
+[support](https://discord.gg/jcs4XwcExv)    -    [invite](https://discord.com/api/oauth2/authorize?client_id=811955068563488788&permissions=133692224&scope=bot)
 
+`)
 
-  .setFooter(Request By ${message.author.tag}, message.author.avatarURL())
-   .setColor("RED");
+  
+   .setColor("BLUE");
    helpEmbed.setTimestamp();
-message.react("<a:emoji_11:814035839956418570>")
+   message.react("<a:emoji_11:814035839956418570>")
     return message.channel.send(helpEmbed).catch(console.error);
 
   }
