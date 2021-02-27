7d97a2fbad2de7dc3ae5ec835ccdf6f5210c6f78
wolfwolf12 <79043462+wolfwolf12@users.noreply.github.com>
2021-02-27 14:41:13 +0300

    Update stop.js

--- Music/stop.js
+++ Music/stop.js
@@ -37,7 +37,7 @@ async execute(message,args,client) {
   await channel.leave();
   //send the approve message
   message.channel.send(new MessageEmbed()
-  .setColor("#c219d8")
+  .setColor("RED")
   .setAuthor(`${message.author.username} stopped the music!`, "https://cdn.discordapp.com/emojis/769915194066862080.png"))
   .catch(console.error);
   }
