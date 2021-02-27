04dee3a8f96166af2b2edd3c9c3f19a43b0745d0
wolfwolf12 <79043462+wolfwolf12@users.noreply.github.com>
2021-02-27 14:37:15 +0300

    Update shuffle.js

--- Music/shuffle.js
+++ Music/shuffle.js
@@ -40,6 +40,6 @@ execute(message,args,client) {
     //send the Approve message
     queue.textChannel.send(new MessageEmbed()
     .setDescription(`**✅ | ${message.author} shuffled the Queue**`)
-    .setColor("#c219d8")).catch(console.error);
+    .setColor("RED")).catch(console.error);
   }
 };
