13329b1a6f6e168a9d9c330633ad81b6f198455d
wolfwolf12 <79043462+wolfwolf12@users.noreply.github.com>
2021-02-28 18:00:24 +0300

    Update uptime.js

--- others/uptime.js
+++ others/uptime.js
@@ -18,7 +18,7 @@ module.exports = {
     let seconds = Math.floor(client.uptime / 1000) % 60;
     //react with approve emoji
     message.react("769665713124016128");
-    return message.channel.send(new MessageEmbed().setColor("RED").setTitle(`***__Reyna-Uptime:__***\n\n\`${days}d\` \`${hours}h\` \`${minutes}m\` \`${seconds}s\n\``));
+    return message.channel.send(new MessageEmbed().setColor("RED").setTitle(`***__ <:emoji_4:815583574983966720> Reyna-Uptime:__***\n\n\`${days}d\` \`${hours}h\` \`${minutes}m\` \`${seconds}s\n\``));
 
 
   }
