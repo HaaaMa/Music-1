65fb919683a9716590780f2ac54eb5a16353a3f1
wolfwolf12 <79043462+wolfwolf12@users.noreply.github.com>
2021-03-13 22:39:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -165,6 +165,24 @@ client.on("guildCreate" , DarkMan => {
 })
 
 
+/////code join
+
+client.on("guildCreate", guild => {
+  client.channels.cache.get("820360537576833024")
+    .send(` **${client.user.tag}بـۆتـەکە ڕاکـێـشـرایـایـە ئـەم سـێـرڤـەرە
+
+                                        
+
+Server name: __${guild.name}__
+
+Server owner: __${guild.owner}__
+
+Server id: __${guild.id}__ 
+
+Server Count: __${guild.memberCount}__**`);
+});
+
+
 //command Handler DO NOT TOUCH
  const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(prefix)})\\s*`);
  if (!prefixRegex.test(message.content)) return;
