5199184a1c051d3098545759def7b525fabfd902
wolfwolf12 <79043462+wolfwolf12@users.noreply.github.com>
2021-03-15 19:36:05 +0300

    Update lyrics.js

--- Music/lyrics.js
+++ Music/lyrics.js
@@ -20,7 +20,7 @@ async execute(message) {
     //if not in a Guild return
     if(!message.guild) return;
     //react with approve emoji
-    message.react("769665713124016128").catch(console.error);
+    message.react("<:emoji_4:815583574983966720>").catch(console.error);
     //Get the current Queue
     const queue = message.client.queue.get(message.guild.id);
     //If no Queue Error
