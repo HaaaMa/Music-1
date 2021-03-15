d35f9961e2c25fa703c89e55c2c91934c516418c
wolfwolf12 <79043462+wolfwolf12@users.noreply.github.com>
2021-03-15 19:32:50 +0300

    Update invite.js

--- others/invite.js
+++ others/invite.js
@@ -13,7 +13,7 @@ module.exports = {
   edesc: "Type this command to get an invite link for the Bot, thanks for every Invite",
   execute(message, args, client) {
     //react with approve emoji
-    message.react("769665713124016128");
+    message.react("<:emoji_4:815583574983966720>");
     //send the invite embed
     message.reply(new MessageEmbed().setColor("#FF0000").setTitle("❤ Please Invite me: ").setDescription("https://discord.com/oauth2/authorize?client_id=806840212608909344&permissions=70346817&scope=bot")
     .setFooter("And enjoy listening to music!", "https://cdn.discordapp.com/avatars/806840212608909344/bf2d9853ffc2b48775c0cf9f8932a189.png?size=1024"));
