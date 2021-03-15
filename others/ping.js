8ca419f475b36f685909bb4fd6f7403f1760d4cf
wolfwolf12 <79043462+wolfwolf12@users.noreply.github.com>
2021-03-15 19:33:09 +0300

    Update ping.js

--- others/ping.js
+++ others/ping.js
@@ -13,7 +13,7 @@ module.exports = {
   edesc: "Type this command to see how fast the Bot can response to your messages / commands inputs!",
   execute(message, args, client) {
     //react with approve emoji
-    message.react("769665713124016128");
+    message.react("<:emoji_4:815583574983966720>");
     //send the Ping embed
     message.reply(new MessageEmbed().setColor("#FF0000").setTitle(":ping_pong: `" + client.ws.ping + "ms`"));
   }
