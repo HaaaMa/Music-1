8325b8b754718f90cc40b241b5714770e522e95a
wolfwolf12 <79043462+wolfwolf12@users.noreply.github.com>
2021-03-15 19:42:16 +0300

    Update search.js

--- Music/search.js
+++ Music/search.js
@@ -23,7 +23,7 @@ async execute(message,args,client) {
      //get serverqueue
      const serverQueue = message.client.queue.get(message.guild.id);
     //react with approve emoji
-    message.react("769665713124016128").catch(console.error);
+    message.react("<:emoji_4:815583574983966720>").catch(console.error);
     //if the argslength is null return error
     if (!args.length)
       return attentionembed(message,`Usage: ${message.client.prefix}${module.exports.name} <Video Name>`)
