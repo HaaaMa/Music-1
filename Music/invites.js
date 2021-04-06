7e590b5ac6dea3150f81f011fa6271db6cc300dd
Dark Man <77631739+DarkMan9054@users.noreply.github.com>
2021-04-06 17:13:32 +0300

    Create invites.js

---
+++ Music/invites.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "invites",
+  aliases: ["userinvites"],
+  enabled: true,            
+  memberPermissions: [ "SEND_MESSAGES" ],            
+  botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],        
+  ownerOnly: false,            
+  cooldown: 3000,
+   async execute(message, args) {
+
+        try {
+            let member = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
+
+            let invites = await message.guild.fetchInvites()
+
+            let memberInvites = invites.filter(i => i.inviter && i.inviter.id === member.user.id);
+
+            if (memberInvites.size <= 0) {
+                return message.channel.send(`**${member.displayName} didn't invite anyone to the server!**`, (member === message.member ? null : member));
+  {}          }
+
+            let content = memberInvites.map(i => i.code).join("\n");
+            let index = 0;
+            memberInvites.forEach(invite => index += invite.uses);
+
+            let embed = new Discord.MessageEmbed()
+                .setColor("#f76fe3")
+                .setFooter(message.guild.name, message.guild.iconURL())
+                .setAuthor(`Invite Tracker for ${message.guild.name}`)
+                .setDescription(`Information on Invites of ${member.displayName}`)
+                .addField("**No. Invited Persons**", index)
+                .addField("Invitation Codes\n\n", content);
+            message.channel.send(embed);
+        } catch (e) {
+            return message.channel.send(e.message)
+        }
+    }
+};
