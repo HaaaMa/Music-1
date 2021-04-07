7aa1c47c306010cf5d72bf6e4c4162937fa1156c
Dark Man <77631739+DarkMan9054@users.noreply.github.com>
2021-04-07 19:12:10 +0300

    Update queue.js

--- Music/queue.js
+++ Music/queue.js
@@ -31,7 +31,7 @@ module.exports = {
     let queueEmbed = new MessageEmbed()
       .setTitle("Music Queue")
       .setDescription(description)
-      .setColor("RED");
+      .setColor("#146DF6");
     //split the description
     const splitDescription = splitMessage(description, {
       maxLength: 2048,
