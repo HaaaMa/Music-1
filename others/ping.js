e51f711b5eee8b36253a9b4ede750ad0868f01b4
Dark Man <77631739+DarkMan9054@users.noreply.github.com>
2021-04-07 15:25:31 +0300

    Update ping.js

--- others/ping.js
+++ others/ping.js
@@ -15,6 +15,6 @@ module.exports = {
     //react with approve emoji
     message.react("<:emoji_4:815583574983966720>");
     //send the Ping embed
-    message.reply(new MessageEmbed().setColor("#FF0000").setTitle(":ping_pong: `" + client.ws.ping + "ms`"));
+    message.reply(new MessageEmbed().setColor("#146DF6").setTitle(":ping_pong: `" + client.ws.ping + "ms`"));
   }
 }
