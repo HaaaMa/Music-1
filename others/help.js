7ef3c4ff0f9041204bfc2b906eb57452639f364e
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-05-17 20:28:43 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -20,24 +20,23 @@ module.exports = {
     .setDescription(`
 
 **User Commands**
-\`${PREFIX}invite\` - \`${PREFIX}support\` - \`${PREFIX}about\`
+\`${PREFIX}invite\` - \`${PREFIX}support\`
 \`${PREFIX}ping\` - \`${PREFIX}prefix\` - \`${PREFIX}uptime\`
 \`${PREFIX}avatar\` - \`${PREFIX}se\` - \`${PREFIX}invites\`
 
 **Music Commands**
-\`${PREFIX}play\` - \`${PREFIX}skip\` - \`${PREFIX}skipto\`
-\`${PREFIX}stop\` - \`${PREFIX}volume\` - \`${PREFIX}nowplaying\`
+\`${PREFIX}play\` - \`${PREFIX}skip\` 
+\`${PREFIX}stop\` - \`${PREFIX}volume\`
 \`${PREFIX}shuffle\` - \`${PREFIX}search\` - \`${PREFIX}resume\`
 \`${PREFIX}remove\` - \`${PREFIX}queue\` - \`${PREFIX}filter\`
 \`${PREFIX}loop\` - \`${PREFIX}lyrics\` - \`${PREFIX}radio\`
 
 **Fun Commands**
 \`${PREFIX}lock\` - \`${PREFIX}unlock\` - \`${PREFIX}ban\`
-\`${PREFIX}unban\` - \`${PREFIX}slowmode\` - \`${PREFIX}giveaway\`
+\`${PREFIX}unban\` - \`${PREFIX}slowmode\`
 
 **Links**
-[support](https://discord.gg/Zj5SFafHKG)    -    [invite](https://discord.com/api/oauth2/authorize?client_id=758785943426564187&permissions=1076132928&scope=bot)
-`)
+[support](https://discord.gg/VWuQfQfjc9)    -    [invite](https://discord.com/api/oauth2/authorize?client_id=814608707412295780&permissions=8&scope=bot)    -    [vote](https://top.gg/bot/814608707412295780)`)
 
    .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
    .setColor("#146DF6");
