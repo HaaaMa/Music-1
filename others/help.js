8ed576d6e3b7d7df483821175fee3cf575d8bd2b
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-05-18 01:44:56 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -15,14 +15,14 @@ module.exports = {
     let commands = message.client.commands.array();
 
     let helpEmbed = new MessageEmbed()
-  //  .setThumbnail(`https://media.discordapp.net/attachments/790836700561670145/843616964106911794/image0.gif`)
+    .setImage(`https://media.discordapp.net/attachments/790836700561670145/843616964106911794/image0.gif`)
     .setAuthor(`Remix`, `https://media.discordapp.net/attachments/790836700561670145/843599820807208970/image0.gif`)
     .setDescription(`
 
 **User Commands**
 \`${PREFIX}invite\`   **-**   \`${PREFIX}support\`   **-**   \`${PREFIX}ping\`
 \`${PREFIX}prefix\`   **-**   \`${PREFIX}uptime\`   **-**   \`${PREFIX}avatar\`
-\`${PREFIX}se (emoji)\`   **-**   \`${PREFIX}invites\`   **-**   \`${PREFIX}embed\`
+\`${PREFIX}se (emoji)\`   **-**   \`${PREFIX}invites\`
 
 **Music Commands**
 \`${PREFIX}play\`   **-**   \`${PREFIX}skip\`   **-**   \`${PREFIX}stop\`   **-**  \`${PREFIX}radio\`
@@ -31,15 +31,15 @@ module.exports = {
 \`${PREFIX}filter\`   **-**   \`${PREFIX}loop\`   **-**  \`${PREFIX}lyrics\`
 
 **Moderation Commands**
-\`${PREFIX}lock\`  **•**  \`${PREFIX}unlock\`  **•** \`${PREFIX}ban\`
-\`${PREFIX}unban\`  **•** \`${PREFIX}slowmode\`
+\`${PREFIX}lock\`  **•**  \`${PREFIX}unlock\`   **-**   \`${PREFIX}ban\`
+\`${PREFIX}unban\`   **•**   \`${PREFIX}slowmode\`
 
 **Links**
 [support](https://discord.gg/VWuQfQfjc9)    -    [invite](https://discord.com/api/oauth2/authorize?client_id=814608707412295780&permissions=8&scope=bot)    -    [vote](https://top.gg/bot/814608707412295780)`)
 
    .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
    .setColor("RANDOM");
-   
+   .setThumbnail(`https://media.discordapp.net/attachments/790836700561670145/838764604443983963/image0.gif`)
    message.react("✅")
     return message.channel.send(helpEmbed).catch(console.error);
 
