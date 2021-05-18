31b0e5b096fdba0bace5c381707fdeb3de424997
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-05-19 00:07:33 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -16,7 +16,7 @@ module.exports = {
 
     let helpEmbed = new MessageEmbed()
   //  .setThumbnail(`https://media.discordapp.net/attachments/790836700561670145/843616964106911794/image0.gif`)
-    .setImage(`https://media.discordapp.net/attachments/790836700561670145/823722035678281758/image0.gif`)
+    .setImage(`https://media.discordapp.net/attachments/790836700561670145/823721979629535233/image0.gif`)
     .setAuthor(`Remix`, `https://media.discordapp.net/attachments/790836700561670145/843599820807208970/image0.gif`)
     .setDescription(`
 
@@ -39,7 +39,7 @@ module.exports = {
 **Links**
 [support](https://discord.gg/VWuQfQfjc9)    -    [invite](https://discord.com/api/oauth2/authorize?client_id=814608707412295780&permissions=8&scope=bot)    -    [vote](https://top.gg/bot/814608707412295780)`)
 
-   .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
+ //  .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
    .setColor("RANDOM");
    message.react("✅")
     return message.channel.send(helpEmbed).catch(console.error);
