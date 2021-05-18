08184950bda16a3d13e2a32b9a22305e537ba19f
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-05-18 04:37:41 +0300

    Update giveaway.js

--- Music/giveaway.js
+++ Music/giveaway.js
@@ -1,62 +1,43 @@
-const { MessageEmbed } = require("discord.js");
-const ms = require("ms");
-module.exports = {
-  name: "giveaway",
-  description: "Create a simple giveaway",
-  cooldown: 1.5,
-  usage: "<time> <winner> <prize>",
-  category: "fun",
-  async execute(message, args) {
-    if (!args[0]) return message.channel.send(`You did not specify your time!`);
-    if (
-      !args[0].endsWith("d") &&
-      !args[0].endsWith("h") &&
-      !args[0].endsWith("m")
-    )
-      return message.channel.send(
-        `You did not use the correct formatting for the time!`
-      );
-    if (isNaN(args[0][0])) return message.channel.send(`That is not a number!`);
-    let channel = message.mentions.channels.first();
-    if (!channel)
-    return message.channel.send(
-        `Please select a channel!`
-      );
-    let prize = args.slice(2).join(" ");
-    if (!prize) return message.channel.send(`No prize specified!`);
-    message.channel.send(`🎉**|Giveaway created in** ${channel}`);
-    let Embed = new MessageEmbed()
-      .setTitle(`🎉 New giveaway!`)
-      .setDescription(
-        `**${prize}**
+client.on("message", message => {
+let command = message.content.toLowerCase().split(" ")[0]
+if(command === `${prefix}giveaway`) {
+  let args = message.content.split(" ").slice(1);
+  if (!args[0]) return message.channel.send(`You did not specify your time!`);
+  if (!args[0].endsWith("d") && !args[0].endsWith("h") && !args[0].endsWith("m"))
+    return message.channel.send(`The time needs to have days (d) or hours (h) or minutes (m)`);
+  if (isNaN(args[0][0])) return message.channel.send(`It must be a number you know that?`);
 
-Host: ${message.author}
-Click 🎉 to win
-`)
-      .setTimestamp()
-      .setFooter(`Start at`)
-      .setColor(`RANDOM`);
-    //delete the Command
-    message.delete({timeout: 300})
-    let m = await channel.send(Embed);
+  let prize = args.slice(1).join(" ");
+  if (!prize) return message.channel.send(`No prize specified!`);
+
+  let embed = new Discord.MessageEmbed()
+    .setTitle(`New giveaway!`)
+    .setDescription(`Host: ${message.author}\nTime: ${args[0]}\nPrize: ${prize}`)
+    .setTimestamp(Date.now() + ms(args[0]))
+    .setColor(`BLUE`);
+  message.channel.send(embed).then(m => {
     m.react("🎉");
-    setTimeout(() => {
-      if (m.reactions.cache.get("🎉").count <= 1) {
-        message.channel.send(`Reactions: ${m.reactions.cache.get("🎉").count}`);
-        return message.channel.send(
-          `Not enough people reacted for me to start draw a winner!`
-        );
-      }
- 
-      let winner = m.reactions.cache
-        .get("🎉")
-        .users.cache.filter((u) => !u.bot)
-        .random();
-      channel.send.(
-    `**The winner of the giveaway for**
- 🎉 ${message.author} 🎉`
-  
-     );
-    }, ms(args[0]));
-  },
-};
+  setTimeout(() => {
+    if (m.reactions.cache.get("🎉").count <= 1) {
+      const embed = new Discord.MessageEmbed()
+      .setColor("RED")
+      .setDescription("No winners")
+      m.edit(embed)
+      return message.channel.send(`Couldnt generate a winner as there is no one in that giveaway!`);
+    }
+
+    let winner = m.reactions.cache.get("🎉").users.cache.filter((b) => !b.bot).random();
+    
+    let embed = new Discord.MessageEmbed()
+    .setColor("GREEN")
+    .setDescription(`Winner: ${winner}`)
+    m.edit(embed)
+    
+    message.channel.send(`The winnder of the giveaway is ${winner}`);
+  }, ms(args[0]));
+}
+)}})
+var vojson = require("./vojson.json");
+function saveBlakJack() {
+    fs.writeFileSync("./vojson.json", JSON.stringify(vojson, null, 4));
+}
