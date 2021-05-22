c5dee16b35d3f65ba1c9e368d4d70ba0e9c36531
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-05-22 16:28:24 +0300

    Create text.js

---
+++ Music/text.js
@@ -0,0 +1,60 @@
+const ytsr = require("youtube-sr")
+const { Client, Collection, MessageEmbed } = require("discord.js");
+const { play } = require("../include/play")
+const { attentionembed } = require("../util/attentionembed");
+const { PREFIX, } = require(`../config.json`);
+////////////////////////////
+//////COMMAND BEGIN/////////
+////////////////////////////
+module.exports = {
+  name: "filter",
+  description: "(fi)Set Audio - Effects",
+  aliases: ["fi"],
+  cooldown: 5,
+  edesc: `Type this Command to change the current audio effect - style \nUsage: ${PREFIX}filter <Filtertype>`,
+
+async execute(message, args, client) {
+
+/// starts
+
+message.delete();
+    let sizes = [
+      "kurd",
+      "roshek",
+      "HaaMa",
+      "juana",
+      "hastaday",
+      "bot dalem law bandana",
+      "dayday",
+      "8=======D",
+      "8========D",
+      "8=========D",
+      "8D",
+      "D",
+      "hama",
+      "esta to lagal ke D",
+      "poli6",
+      "wa arOm chetr nambenawa",
+      "Rozhek darom",
+      "Dndbs",
+      "hdhwhd"
+    ];
+
+    let Member =
+      message.mentions.members.first() ||
+      message.guild.members.cache.get(args[0]) ||
+      message.member;
+
+    let Result = sizes[Math.floor(Math.random() * sizes.length)];
+
+    let embed = new MessageEmbed()
+      .setColor(Color)
+      .setTitle(``)
+      .setDescription(`**\n${Result}**`)
+   // .setFooter(`Requested by ${message.author.username}`)
+  //    .setTimestamp();
+    message.channel.send(embed);
+
+    //End
+  }
+};
