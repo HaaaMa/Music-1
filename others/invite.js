025904dd4ff63f885f48298656a778d92b28b230
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-05-26 17:52:04 +0300

    Update invite.js

--- others/invite.js
+++ others/invite.js
@@ -13,7 +13,7 @@ module.exports = {
   edesc: "Type this command to get an invite link for the Bot, thanks for every Invite",
   execute(message, args, client) {
     //react with approve emoji
-    message.react("<a:setting:813404135181385759>");
+    message.react("✅");
     //send the invite embed
     message.reply(new MessageEmbed().setColor("#146DF6").setTitle("Remix Bot Invite ✨").setDescription("<a:waitting:823649927556628481> **[Click Here](https://discord.com/api/oauth2/authorize?client_id=814608707412295780&permissions=8&scope=bot) to invite the bot.**")
     .setFooter(""));
