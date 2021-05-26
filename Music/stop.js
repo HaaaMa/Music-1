0bc1dfc096f496b8f703d8efb8ef4f0f50588c0d
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-05-26 17:24:45 +0300

    Update stop.js

--- Music/stop.js
+++ Music/stop.js
@@ -19,7 +19,7 @@ async execute(message,args,client) {
   //if not in a guild retunr
   if (!message.guild) return;
   //react with approve emoji
-  message.react("✔️").catch(console.error);
+  message.react("✅").catch(console.error);
   const { channel } = message.member.voice;
   //get the serverQueue
   const queue = message.client.queue.get(message.guild.id);
