fe6deabc52c459492dbecc595badffb3f7a1a07a
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-05-26 17:53:46 +0300

    Update ping.js

--- others/ping.js
+++ others/ping.js
@@ -13,7 +13,7 @@ module.exports = {
   edesc: "Type this command to see how fast the Bot can response to your messages / commands inputs!",
   execute(message, args, client) {
     //react with approve emoji
-    message.react("<:emoji_4:815583574983966720>");
+    message.react("📶");
     //send the Ping embed
     message.reply(new MessageEmbed().setColor("#146DF6").setTitle(":ping_pong: `" + client.ws.ping + "ms`"));
   }
