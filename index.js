c25dd0e31e81cd346823931353cecd2f3cb123bc
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-06-02 11:10:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('');
+client.login('ODE0NjA4NzA3NDEyMjk1Nzgw.YDgVeg.q_fdAMQqspFm0kHnwMphEc9VBJ4');
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
