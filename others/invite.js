27bee3de556e8fff0b157fa2298013a1bc3759f8
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-06-03 20:05:19 +0300

    Update invite.js

--- others/invite.js
+++ others/invite.js
@@ -15,7 +15,8 @@ module.exports = {
     //react with approve emoji
     message.react("✅");
     //send the invite embed
-    message.reply(new MessageEmbed().setColor("#146DF6").setTitle("Remix Bot Invite ✨").setDescription("<a:waitting:823649927556628481> **[Click Here](https://discord.com/api/oauth2/authorize?client_id=814608707412295780&permissions=8&scope=bot) to invite the bot.**")
-    .setFooter(""));
+    message.reply(new MessageEmbed().setColor("#146DF6").setTitle("Remix Bot Invite ✨").setDescription("✔️**[Click Here](https://discord.com/api/oauth2/authorize?client_id=814608707412295780&permissions=8&scope=bot) to invite the bot.**")
+   .setImage("https://media.discordapp.net/attachments/790836700561670145/838769104718266381/image0.gif");
+   .setFooter(""));
   }
 }
