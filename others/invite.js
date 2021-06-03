3df578e3f43a67254bac3e87001060768226abed
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-06-03 20:37:45 +0300

    Update invite.js

--- others/invite.js
+++ others/invite.js
@@ -1,22 +1,21 @@
 const { Client, Collection, MessageEmbed } = require(`discord.js`);
 const {
-  PREFIX,
-  approveemoji,
-  denyemoji
+  PREFIX,
+  approveemoji,
+  denyemoji
 } = require(`../config.json`);
 
 module.exports = {
-  name: `invite`,
-  description: `Gives you an invite`,
-  aliases: ["add"],
-  cooldown: 3,
-  edesc: "Type this command to get an invite link for the Bot, thanks for every Invite",
-  execute(message, args, client) {
-    //react with approve emoji
+  name: `invite`,
+  description: `Gives you an invite`,
+  aliases: ["add"],
+  cooldown: 3,
+  edesc: "Type this command to get an invite link for the Bot, thanks for every Invite",
+  execute(message, args, client) {
+    //react with approve emoji
     message.react("✅");
-    //send the invite embed
-    message.reply(new MessageEmbed().setColor("#146DF6").setTitle("Remix Bot Invite ✨").setDescription("✔️**[Click Here](https://discord.com/api/oauth2/authorize?client_id=814608707412295780&permissions=8&scope=bot) to invite the bot.**")
-   .setImage("https://media.discordapp.net/attachments/790836700561670145/838769104718266381/image0.gif")
-   .setFooter(""));
-  }
+    //send the invite embed
+    message.reply(new MessageEmbed().setColor("#146DF6").setTitle("Remix Bot Invite ✨").setDescription("✔️ **[Click Here](https://discord.com/api/oauth2/authorize?client_id=814608707412295780&permissions=8&scope=bot) to invite the bot.**")
+    .setFooter(""));
+  }
 }
