902f703e4b4b6cd34a8a2f3d9f6ec6d78179dd50
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-06-04 08:13:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ client.on(`ready`, () => {
       });
 
     
-    client.user.setActivity(`${PREFIX}help | ${client.guilds.cache.size} Server,Users ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)},`, { type: "Playing"});
+    client.user.setActivity(`${PREFIX}help | ${client.guilds.cache.size} Server,Users ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)},`, { type: "PLAYING"});
    
   
       }, (5000));
